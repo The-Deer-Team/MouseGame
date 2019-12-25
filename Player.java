4302d2377d5e8e15c8adbdd0b9775f6b1a008332
HSMF <59176979+HSMF@users.noreply.github.com>
2019-12-25 22:43:11 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,9 +2,52 @@
 player
 */
 
-public class Player {
 
-    public Player(){
+public class Player extends Entity {
+
+    public Player() {
+        x = 400;
+        y = 400;
+        posX = (int) x;
+        posY = (int) y;
+        speedX = 0;
+        speedY = 0;
+        width = 20;
+        height = 50;
+        maxSpeedX = 10;
+        maxSpeedY = 40;
+    }
+
+
+    public void IamSpeed(int key) {
+        switch (key) {
+        case 1: // w
+            speedY -= 20; // pls modify those vals
+            break;
+        case 2: // a
+            speedX -= 5;
+            break;
+        case 3: // s
+            //speedY -= 5;
+            break;
+        case 4: // d
+            speedX += 5;
+            break;
+        default:
+            break;
+        }
+        if (key!=0){
+        System.out.println(key+" "+speedX+" , "+speedY+";   "+x+", "+y+";  "+posX+", "+posY);
+        }
+    }
+
+    public void update() {
+        //gravity();
+        friction();
+        move();
+        adjustPosition();
+    }
+        
 /*
 In this farewell
 There's no blood, there's no alibi
@@ -47,5 +90,5 @@ Forgiving what I've done
 (Na-na na na)
 (Na-na na)
 */
-    }
+
 }
